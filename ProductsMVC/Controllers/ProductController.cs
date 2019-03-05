bdeafc4213af08fa79669ef7921db54b2862f87e
gusadolfo123 <gusadolfo123@hotmail.com>
2019-03-05 06:06:37 -0500

    Implementacion Detail

--- ProductsMVC/Controllers/ProductController.cs
+++ ProductsMVC/Controllers/ProductController.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProductsMVC.Contracts;
 
 namespace ProductsMVC.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly IRepository _Repository;
+
+        public ProductController(IRepository repository)
+        {
+            _Repository = repository;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var products = _Repository.GetList();
+            return View(products);
+        }
+
+
+        public IActionResult Details(int id)
+        {
+            var product = _Repository.GetProductByID(id);
+            return View(product);
         }
     }
 }
